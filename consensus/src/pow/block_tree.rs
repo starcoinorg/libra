13361140225ee9d8101bd9cb8fbb53381c4508cf
tiangong3624749 <tiangong3624749@163.com>
2019-11-18 19:11:41 +0800

    impl BlockInfo.

--- consensus/src/pow/block_tree.rs
+++ consensus/src/pow/block_tree.rs
@@ -1,10 +1,11 @@
 use std::collections::{HashMap, HashSet};
 use libra_crypto::HashValue;
 use executor::ProcessedVMOutput;
+use failure::Result;
 
 ///
 /// ```text
-///   Committed(B4)-> B5  -> B6  -> B7
+///   Committed(B4) --> B5  -> B6  -> B7
 ///                |
 ///                └--> B5' -> B6' -> B7'
 ///                            |
@@ -13,17 +14,47 @@ use executor::ProcessedVMOutput;
 /// height: B7 B7' B7"
 /// root: B7
 /// begin: B4
-struct BlockTree {
+pub struct BlockTree {
     height: u64,
     root: HashValue,
     id_to_block: HashMap<HashValue, BlockInfo>,
     begin: HashValue,
 }
 
+impl BlockTree {
+    pub fn add_block_info(&self) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn prune(&self) {
+        unimplemented!()
+    }
+
+    fn height(&self) -> u64 {
+        self.height
+    }
+
+    fn root(&self) -> HashValue {
+        unimplemented!()
+    }
+
+    pub fn find_output_by_id(&self, id: HashValue) -> Result<BlockInfo> {
+        unimplemented!()
+    }
+}
+
 /// Can find parent block or children block by BlockInfo
-struct BlockInfo {
+pub struct BlockInfo {
     id: HashValue,
     parent_id : HashValue,
     children: HashSet<HashValue>,
     vm_output: ProcessedVMOutput,
 }
+
+impl BlockInfo {
+
+    fn append_children(&mut self, children_id: HashValue) {
+        self.children.insert(children_id);
+    }
+}
+
