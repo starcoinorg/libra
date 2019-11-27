4ebe489aa4ef5353e193fd643404f5df4f5ed70c
tiangong3624749 <tiangong3624749@163.com>
2019-11-27 15:25:51 +0800

    remove block index from consensus.

--- consensus/src/chained_bft/consensusdb/schema/mod.rs
+++ consensus/src/chained_bft/consensusdb/schema/mod.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub(crate) mod block;
-pub mod block_index;
 pub(crate) mod quorum_certificate;
 pub(crate) mod single_entry;
 
@@ -12,7 +11,6 @@ use schemadb::ColumnFamilyName;
 pub(super) const BLOCK_CF_NAME: ColumnFamilyName = "block";
 pub(super) const QC_CF_NAME: ColumnFamilyName = "quorum_certificate";
 pub(super) const SINGLE_ENTRY_CF_NAME: ColumnFamilyName = "single_entry";
-pub(super) const BLOCK_INDEX_CF_NAME: ColumnFamilyName = "block_index";
 
 fn ensure_slice_len_eq(data: &[u8], len: usize) -> Result<()> {
     ensure!(
