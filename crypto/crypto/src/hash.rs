9c4ab54d4c6355b663421e562e0866a95a88978a
jolestar <jolestar@gmail.com>
2020-03-29 02:54:17 +0800

    Fix HashValue serialize bug for json rpc.

--- crypto/crypto/src/hash.rs
+++ crypto/crypto/src/hash.rs
@@ -286,8 +286,9 @@ impl<'de> de::Deserialize<'de> for HashValue {
         D: de::Deserializer<'de>,
     {
         if deserializer.is_human_readable() {
-            let encoded_hash = <&str>::deserialize(deserializer)?;
-            HashValue::from_hex(encoded_hash).map_err(<D::Error as ::serde::de::Error>::custom)
+            let encoded_hash = ::serde::private::de::borrow_cow_str(deserializer)?;
+            HashValue::from_hex(encoded_hash.as_ref())
+                .map_err(<D::Error as ::serde::de::Error>::custom)
         } else {
             // See comment in serialize.
             #[derive(::serde::Deserialize)]
