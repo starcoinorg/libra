00fdaa0e11384cf4be15769751b9e05e39e5889a
Guangyu Zhu <guangyuz@gmail.com>
2021-02-26 10:19:11 +0800

    refactor x tools and pass project root to x-context

--- devtools/x/src/config.rs
+++ devtools/x/src/config.rs
@@ -200,8 +200,8 @@ impl XConfig {
         toml::from_slice(bytes).map_err(Into::into)
     }
 
-    pub fn from_project_root() -> Result<Self> {
-        Self::from_file(project_root().join("x.toml"))
+    pub fn from_project_root(project_root: &'static Path) -> Result<Self> {
+        Self::from_file(project_root.join("x.toml"))
     }
 }
 
